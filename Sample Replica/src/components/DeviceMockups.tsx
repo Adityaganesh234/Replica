96138aee3d901f11329cc006a8f60754306322f2
Adityaganesh234 <adityaganesh.chaganti@xwola.com>
2025-08-12 20:03:18 +0530

    Image position
    Corrected Image position.

--- Sample Replica/src/components/DeviceMockups.tsx
+++ Sample Replica/src/components/DeviceMockups.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 export default function DeviceMockups(): JSX.Element {
   return (
-    <div className="mx-auto max-w-4xl">
+    <div className="mx-auto max-w-lg">
       {/* <div className="overflow-hidden rounded-2xl border border-gray-200 shadow-md"> */}
       <div className="overflow-hidden"> 
         <img
           src="https://i0.wp.com/bravegymmarketing.com/wp-content/uploads/2023/08/mockup.png?resize=800%2C492&ssl=1"
           alt="Responsive website mockups on phone, tablet, and desktop"
-          className="block h-84 w-96"
+          className="block h-82 w-90"
           loading="lazy"
         />
       </div>
